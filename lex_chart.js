fdff103c989009d7cafa72ee164ca0f87e60de02
elsage <parker.spl@gmail.com>
2016-01-23 22:24:37 -0600

    Line Plotting / Interpolation Working

--- lex_chart.js
+++ lex_chart.js
@@ -1,49 +1,59 @@
-var dataset = [ [5, 20], [480, 90], [250, 50], [100, 33], [330, 95], [410, 12], [475, 44], [25, 67], [85, 21], [220, 88] ];
+
+d3.csv("data/measures.csv", function(error, data) {
+	data.forEach(function(d) {
+		d.Height = +d.Height;
+		d.Weight = +d.Weight;
+	});
 
 
+	var padding = 50, w = window.innerWidth - 100, h = window.innerHeight - 100;
 
-var padding = 25, w = window.innerWidth - 100, h = window.innerHeight - 100;
+	var xScale = d3.scale.linear()
+							.domain([0, d3.max(data, function(d) { return d.Height; })])
+							.range([padding, w - padding]);
 
-var xScale = d3.scale.linear()
-						.domain([0, d3.max(dataset, function(d) { return d[0]; })])
-						.range([padding, w - padding]);
+	var yScale = d3.scale.linear()
+							.domain([0, d3.max(data, function(d) { return d.Weight; })])
+							.range([h - padding, padding]);
 
-var yScale = d3.scale.linear()
-						.domain([0, d3.max(dataset, function(d) { return d[1]; })])
-						.range([h - padding, padding]);
+	var xAxis = d3.svg.axis()
+						.scale(xScale)
+						.orient("bottom");
 
-var rScale = d3.scale.linear()
-						.domain([0, d3.max(dataset, function(d) { return d[1]; })])
-						.range([0, 5]);
+	var yAxis = d3.svg.axis()
+						.scale(yScale)
+						.orient("left");
 
-var xAxis = d3.svg.axis()
-					.scale(xScale)
-					.orient("bottom");
+	var svg = d3.select("#chart")
+				.append("svg")
+				.attr("width", w)
+				.attr("height", h);
 
-var yAxis = d3.svg.axis()
-					.scale(yScale)
-					.orient("left");
+	var line_svg = d3.svg.line()
+							.interpolate("basis")
+							.x(function(d) { return xScale(d.Height); })
+							.y(function(d) { return yScale(d.Weight); });
 
-var svg = d3.select("#chart")
-			.append("svg")
-			.attr("width", w)
-			.attr("height", h);
+	svg.append("path")
+		.attr("class", "line_path")
+		.attr("d", line_svg(data)); 
 
-svg.selectAll("circle")
-	.data(dataset)
-	.enter()
-	.append("circle")
-	.attr("cx", function(d) { return xScale(d[0]); })
-	.attr("cy", function(d) { return yScale(d[1]); })
-	.attr("r", function(d) { return rScale(d[1])});
+	svg.selectAll("circle")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr("cx", function(d) { return xScale(d.Height) })
+		.attr("cy", function(d) { return yScale(d.Weight) })
+		.attr("r", 5);
 
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(0," + (h - padding) + ")")
+		.call(xAxis);
 
-svg.append("g")
-	.attr("class", "axis")
-	.attr("transform", "translate(0," + (h - padding) + ")")
-	.call(xAxis);
+	svg.append("g")
+		.attr("class", "axis")
+		.attr("transform", "translate(" + padding + ",0)")
+		.call(yAxis);
 
-svg.append("g")
-	.attr("class", "axis")
-	.attr("transform", "translate(" + padding + ",0)")
-	.call(yAxis);
+})
